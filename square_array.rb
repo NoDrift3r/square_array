eb2f4e31ac15384fe112fc2ebfaabf66a9c5787c
Jesus <jose.jesus16@gmail.com>
2021-03-07 15:53:57 -0500

    commit tree 67c21d4731997040a370e05557a30dab15fa790a

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  
+ new_array = []
+ array.each do |number|
+ 
+ new_array<<number * number
+
+ puts "Jesus loves this number #{number}!"
+end
+new_array
 end
